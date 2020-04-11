a204291577a6a9261b00c9eab3d49e1c094556e3
Enrique Mapica Calderón <kikemalpi@gmail.com>
2020-04-11 22:20:53 +0200

    commit 2 SecondBranch

--- src/cifrado/Main.java
+++ src/cifrado/Main.java
@@ -2,6 +2,7 @@ package cifrado;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Vamos a empezar....");
 		CodigoCesar cifrar = new CodigoCesar();
 		cifrar.pedirFrase();
 	}
